d2d1ad2ab497eb223505e4370e6f116963537431
Mike Moore <23637288+mmoore0011@users.noreply.github.com>
2017-07-18 22:27:24 -0500

    Update makecorevm.rb

--- makecorevm.rb
+++ makecorevm.rb
@@ -55,16 +55,14 @@ Trollop.die("must specify default gw for vm") unless opts[:gw]
 Trollop.die("must specify domain name server for vm") unless opts[:dns]
 Trollop.die("must specify resource pool") unless opts[:pool]
 VM_name = ARGV[0] or abort "must specify VM name"
-#########
+
 
 ######### These 2 lines connect to the vsphere and get the datacenter object ########
 vim = VIM.connect opts
 DC = vim.serviceInstance.find_datacenter(opts[:datacenter]) or abort "datacenter not found"
-#########
+
 
 ######### Routine to get the resource pool name.  #########
-######### This seems like WAY overkill but I haven't been able to find another method that works #########
-
 def find_pool(poolName)
   base_entity = DC.hostFolder
   entity_array = poolName.split('/')
@@ -87,13 +85,14 @@ def find_pool(poolName)
   base_entity = base_entity.resourcePool if !base_entity.is_a?(RbVmomi::VIM::ResourcePool) && base_entity.respond_to?(:resourcePool)
   base_entity
 end
-#########
+
 
 ######### These 2 lines find the folder as specified with --folder and set it to vmFolder #########
 root_vm_folder = DC.vmFolder
 vmFolder = root_vm_folder.traverse(opts[:folder], VIM::Folder)
-#########
 
+
+######### Find the template #########
 Template=vim.serviceInstance.find_datacenter.find_vm(template_name) or abort ("Template " + template_name + " Not Found!")
 
 ######### Define spec #########
@@ -104,12 +103,12 @@ Spec = VIM.VirtualMachineCloneSpec(
   :location => relocateSpec,
   :powerOn => false,
   :template => false)
-#########
+
 
 ######## clone from template coreos_production_vmware_ova #########
 task = Template.CloneVM_Task(:folder => vmFolder, :name => VM_name, :spec => Spec).wait_for_completion
 puts "creating " + VM_name +  "..."
-#########
+
 
 ######### Find the VM you just created and call it NewVM #########
 children = vmFolder.children.find_all
@@ -118,15 +117,13 @@ children.each do |child|
     NewVM = child
   end
 end
-#########
 
 puts VM_name + " created. Sending custom config..."
 
-######### Define hash with guestinfo params #########
 ######### base64 encode the cloudconfig into a giant string #########
 encoded_string = Base64.strict_encode64(File.open(opts[:config]).to_a.join)
 
-######### define rest of guestinfo #########
+######### Define hash with guestinfo params #########
 Guestinfo = Hash.new
 Guestinfo["guestinfo.hostname"] = VM_name
 Guestinfo["guestinfo.interface.0.route.0.destination"] = "0.0.0.0/0"
@@ -138,18 +135,18 @@ Guestinfo["guestinfo.coreos.config.data.encoding"] = "base64"
 Guestinfo["guestinfo.interface.0.name"] = "ens192"
 Guestinfo["guestinfo.interface.0.ip.0.address"] = opts[:ip]
 Guestinfo["guestinfo.coreos.config.data"] = encoded_string
-#########
+
 
 ######### Pump the guestinfo params into the new vm #########
 Guestinfo.each do |key, value|
   extraConfig = [{ :key => key, :value => value}]
   NewVM.ReconfigVM_Task(:spec => VIM.VirtualMachineConfigSpec(:extraConfig => extraConfig)).wait_for_completion
 end
-#########
+
 
 ######### Power on the new vm #########
 NewVM.PowerOnVM_Task.wait_for_completion
-#########
+
 
 ######### The option --list_vms has nothing to do with this script but dealing with folders using rbvmomi is painful #########
 ######### so leaving this as an example #########
@@ -168,5 +165,5 @@ if opts[:list_vms]
   # list all vms in the folder specified with --folders
   list_vms(vmFolder)
 end
-#########
+
 
